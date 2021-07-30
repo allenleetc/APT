f8f01156d8bec225d43c120fbc487a10c3a6c320
Allen Lee <al@leapscientific.com>
2021-07-29 22:21:21 -0400

    ci proj

---
+++ makehorseproj.m
@@ -0,0 +1,16 @@
+function makehorseproj(varargin)
+mov = varargin{1};
+lObj = StartAPT;
+cfg = Labeler.cfgGetLastProjectConfigNoView;
+cfg.NumLabelPoints = 3;
+cfg.NumViews = 1;
+cfg.MultiAnimal = 0;
+lObj.initFromConfig(cfg);
+lObj.projNew('horse');
+lObj.movieAdd(mov);
+lObj.movieSet(1);
+lObj.setFrame(5);
+lObj.labelPosSet([40 50;50 60;80 100]);
+lblname = fullfile(pwd,'horseproj.lbl');
+lObj.projSaveRaw(lblname);
+disp(lObj.projFSInfo.filename);
